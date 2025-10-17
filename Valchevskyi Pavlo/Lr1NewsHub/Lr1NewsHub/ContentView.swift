6bfc2bbeb84a4d2633dc942e9e9257b242b56ab0
Pavlo Valchevskyi <pavlo.valchevskyi.oi.2023@lpnu.ua>
2025-10-17 12:10:11 -0700

    Add struct Article, state variable of news and variable of category.

--- Valchevskyi Pavlo/Lr1NewsHub/Lr1NewsHub/ContentView.swift
+++ Valchevskyi Pavlo/Lr1NewsHub/Lr1NewsHub/ContentView.swift
@@ -7,7 +7,24 @@
 
 import SwiftUI
 
+struct Article: Identifiable {
+    let id = UUID()
+    let title: String
+    let text: String
+    let category: String
+    var isSaveOffline: Bool = false
+}
+
 struct ContentView: View {
+    @State var selectedCategory: String = "All"
+    
+    @State var news: [Article] = [
+        Article(title: "Tittle article 1", text: "Text article 1", category: "Category 1"),
+        Article(title: "Tittle article 2", text: "Text article 2", category: "Category 2")
+    ]
+    
+    let categories = ["All", "Category 1", "Category 2"]
+
     var body: some View {
         HStack {
             Image(systemName: "globe")
