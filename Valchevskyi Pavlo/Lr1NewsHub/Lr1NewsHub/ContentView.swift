17308422628c6ca24618393e8807fd1ae429765d
Pavlo Valchevskyi <pavlo.valchevskyi.oi.2023@lpnu.ua>
2025-10-17 11:19:57 -0700

    Intial commit for lr1

---
+++ Valchevskyi Pavlo/Lr1NewsHub/Lr1NewsHub/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  Lr1NewsHub
+//
+//  Created by Pavlo on 17.10.2025.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
