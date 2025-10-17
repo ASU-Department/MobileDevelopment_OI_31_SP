bce22c1a296613d3650396de61f22691aaee5fe7
ky3ow <vova2341591@gmail.com>
2025-10-17 09:59:26 +0300

    scaffold the project for task1

---
+++ Volodymyr Havryliuk/Task1/Task1/Task1App.swift
@@ -0,0 +1,17 @@
+//
+//  Task1App.swift
+//  Task1
+//
+//  Created by v on 17.10.2025.
+//
+
+import SwiftUI
+
+@main
+struct Task1App: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
