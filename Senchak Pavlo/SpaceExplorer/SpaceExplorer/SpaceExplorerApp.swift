43f72f3d6b65b3455018817d3410ed6eb5c07bb9
Pab1m <pab1m@Mac-Pab1m.local>
2025-10-20 00:23:16 +0300

    Add Lab1 UI (SpqceExplorer)

---
+++ Senchak Pavlo/SpaceExplorer/SpaceExplorer/SpaceExplorerApp.swift
@@ -0,0 +1,17 @@
+//
+//  SpaceExplorerApp.swift
+//  SpaceExplorer
+//
+//  Created by Pab1m on 19.10.2025.
+//
+
+import SwiftUI
+
+@main
+struct SpaceExplorerApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
