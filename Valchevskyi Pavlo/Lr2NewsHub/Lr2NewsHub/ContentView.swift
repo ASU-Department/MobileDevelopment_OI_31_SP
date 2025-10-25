22210255d9d171b9ceb4e5127cb11bef7d0544c9
Pavlo Valchevskyi <pavlo.valchevskyi.oi.2023@lpnu.ua>
2025-10-25 12:40:40 -0700

    Initial commit for LR2NewsHub

---
+++ Valchevskyi Pavlo/Lr2NewsHub/Lr2NewsHub/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  Lr2NewsHub
+//
+//  Created by Pavlo on 25.10.2025.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ Valchevskyi Pavlo/Lr2NewsHub/Lr2NewsHub/Lr2NewsHubApp.swift
@@ -0,0 +1,17 @@
+//
+//  Lr2NewsHubApp.swift
+//  Lr2NewsHub
+//
+//  Created by Pavlo on 25.10.2025.
+//
+
+import SwiftUI
+
+@main
+struct Lr2NewsHubApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
