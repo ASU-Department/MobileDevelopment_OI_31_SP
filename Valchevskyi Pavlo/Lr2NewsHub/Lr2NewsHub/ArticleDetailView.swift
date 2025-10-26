2114b328b3d12fa09c6d2789b891c925c91cb484
Pavlo Valchevskyi <pavlo.valchevskyi.oi.2023@lpnu.ua>
2025-10-25 20:27:47 -0700

    Modify struct Arcticle (add userPoint for rating article) and add Slider (UIViewRepresentable).

--- Valchevskyi Pavlo/Lr2NewsHub/Lr2NewsHub/ArticleDetailView.swift
+++ Valchevskyi Pavlo/Lr2NewsHub/Lr2NewsHub/ArticleDetailView.swift
@@ -49,6 +49,14 @@ struct ArticleDetailView: View {
                 Section(header: Text("Save Offline Article").bold()) {
                     Toggle("Save offline", isOn: $article.isSaveOffline)
                 }
+                
+                Section(header: Text("Rate Article").bold()) {
+                    VStack {
+                        Text("Point: \(article.userPoint)")
+                        SliderView(userPoint: $article.userPoint)
+                            .frame(height: 25)
+                    }
+                }
             }
             .border(Color.blue, width: 1)
             
@@ -86,3 +94,35 @@ struct ShareArticle: UIViewControllerRepresentable {
     ))
 }
 
+struct SliderView: UIViewRepresentable {
+    @Binding var userPoint: Int
+    
+    func makeUIView(context: Context) -> UISlider {
+        let slider = UISlider(frame: .zero)
+        slider.minimumValue = 0
+        slider.maximumValue = 5
+        slider.value = Float(userPoint)
+        slider.addTarget(context.coordinator, action: #selector(Coordinator.valueChanged(_:)), for: .valueChanged)
+        return slider
+    }
+    
+    func updateUIView(_ uiView: UISlider, context: Context) {
+        uiView.value = Float(userPoint)
+    }
+    
+    func makeCoordinator() -> Coordinator {
+        Coordinator(value: $userPoint)
+    }
+    
+    class Coordinator: NSObject {
+        var value: Binding<Int>
+        
+        init(value: Binding<Int>) {
+            self.value = value
+        }
+        
+        @objc func valueChanged(_ sender: UISlider) {
+            value.wrappedValue = Int(sender.value)
+        }
+    }
+}
