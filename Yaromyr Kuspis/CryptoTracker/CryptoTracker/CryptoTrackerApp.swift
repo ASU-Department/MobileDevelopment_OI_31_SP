edde9b6be6a644f1e1c9b85ddb8b44e53fc48583
YarikKryt <kuspis.yaromir@gmail.com>
2025-11-02 12:11:21 +0200

    Initial commit

---
+++ Yaromyr Kuspis/CryptoTracker/CryptoTracker/CryptoTrackerApp.swift
@@ -0,0 +1,17 @@
+//
+//  CryptoTrackerApp.swift
+//  CryptoTracker
+//
+//  Created by Яромир-Олег Куспісь on 02.11.2025.
+//
+
+import SwiftUI
+
+@main
+struct CryptoTrackerApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
