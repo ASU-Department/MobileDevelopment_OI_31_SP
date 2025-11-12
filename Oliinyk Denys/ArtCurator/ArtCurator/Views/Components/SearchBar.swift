12bb555ef8916feff2eb52e7e6979c5500342eed
Denys Oliinyk <denysoliinyk@outlook.com>
2025-11-12 18:28:28 +0200

    feat: implemented lab2 uikit components usage with navigation and artwork details view

--- Oliinyk Denys/ArtCurator/ArtCurator/Views/Components/SearchBar.swift
+++ Oliinyk Denys/ArtCurator/ArtCurator/Views/Components/SearchBar.swift
@@ -12,14 +12,17 @@ struct SearchBar: View {
     @Binding var filterFavorites: Bool
     
     var body: some View {
-        VStack {
+        VStack(spacing: 12) {
             TextField("Search artist or title", text: $searchQuery)
                 .padding(10)
                 .background(.gray.opacity(0.2))
                 .cornerRadius(8)
             
             Toggle("Filter Favorites", isOn: $filterFavorites)
+                .toggleStyle(SwitchToggleStyle(tint: .red))
         }
         .padding()
+        .background(Color(.systemBackground))
+        .shadow(color: .black.opacity(0.05), radius: 5, y: 2)
     }
 }
