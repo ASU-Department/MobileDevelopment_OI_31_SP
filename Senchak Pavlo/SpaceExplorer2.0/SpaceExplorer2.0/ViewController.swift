bf638fdd04181645ae8542ea7c516c0d7c1d1675
Pab1m <pab1m@Mac-Pab1m.local>
2025-11-16 18:52:35 +0200

    Add storyboard-based UI setup for Lab2

---
+++ Senchak Pavlo/SpaceExplorer2.0/SpaceExplorer2.0/ViewController.swift
@@ -0,0 +1,16 @@
+//
+//  ViewController.swift
+//  delete
+//
+//  Created by Pab1m on 16.11.2025.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+}
