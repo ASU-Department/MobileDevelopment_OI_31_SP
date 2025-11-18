8cae4414dff06f53b037082c6648f7a3234ef537
A-Z pack group <a-zpackgroup@MacBook-Air-A-Z.local>
2025-11-18 20:01:58 +0200

    feat:add init for exerciseItem to genarate UUID for id

--- Oleksii Kaparov/lab1/lab1/ExerciseModel.swift
+++ Oleksii Kaparov/lab1/lab1/ExerciseModel.swift
@@ -7,8 +7,15 @@
 import Foundation
 
 struct ExerciseItem: Identifiable, Hashable {
-    let id = UUID()
+    let id: UUID
     var name: String
     var sets: Int
     var reps: Int
+    
+    init(id: UUID = UUID(), name: String, sets: Int, reps: Int) {
+        self.id = id
+        self.name = name
+        self.sets = sets
+        self.reps = reps
+    }
 }
