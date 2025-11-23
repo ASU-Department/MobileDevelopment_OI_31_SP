ac459f605c8d09adc5c2634054daf8be4fa48715
Mornacael <jaskiv796@gmail.com>
2025-11-23 22:55:37 +0200

    feat: Implement networking, persistence, and settings (Lab 3)

--- WeatherWise/WeatherWise/WeatherWiseApp.swift
+++ WeatherWise/WeatherWise/WeatherWiseApp.swift
@@ -1,11 +1,5 @@
-//
-//  WeatherWiseApp.swift
-//  WeatherWise
-//
-//  Created by vburdyk on 22.11.2025.
-//
-
 import SwiftUI
+import SwiftData
 
 @main
 struct WeatherWiseApp: App {
@@ -13,5 +7,6 @@ struct WeatherWiseApp: App {
         WindowGroup {
             ContentView()
         }
+        .modelContainer(for: HistoricalEvent.self)
     }
 }
