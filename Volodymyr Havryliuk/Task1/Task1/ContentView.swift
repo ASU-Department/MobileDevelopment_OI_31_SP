08cf6ac18bae7b012ac5cf966e19a42d69fea46d
ky3ow <vova2341591@gmail.com>
2025-11-23 03:06:31 +0200

    feat: add basic UIKit integration, add sample Nav stack

--- Volodymyr Havryliuk/Task1/Task1/ContentView.swift
+++ Volodymyr Havryliuk/Task1/Task1/ContentView.swift
@@ -15,27 +15,66 @@ struct Book: Hashable, Identifiable {
 
 struct ContentView: View {
     @State var books: [Book] = [
-        Book(title: "book 1"),
-        Book(title: "book 2")
     ]
+    @State private var path = NavigationPath()
+    
+    @State var motivation = "You have nothing"
+    
+    let quotes = [
+        "Reading is dreaming with open eyes.",
+        "So many books, so little time.",
+        "A room without books is like a body without a soul.",
+    ]
+    
     var body: some View {
-        VStack {
-            Image(systemName: "globe")
-                .imageScale(.large)
-                .foregroundStyle(.tint)
-            Text("BookTracker")
-                .bold()
-                .font(.system(size: 36))
-            List {
-                ForEach($books) { $book in
-                    BookRow(book: $book)
+        NavigationStack(path: $path) {
+            VStack {
+                if books.isEmpty {
+                    SimpleText(text: motivation)
+                        .frame(height: 80)
+                        .padding(.horizontal)
+                    
+                    Button("Get motivated") {
+                        motivation = quotes.randomElement() ?? "Something is very wrong, someone stole all quotes"
+                    }
+                    .buttonStyle(.borderedProminent)
+                } else {
+                    Image(systemName: "globe")
+                        .imageScale(.large)
+                        .foregroundStyle(.tint)
+                    Text("BookTracker")
+                        .bold()
+                        .font(.system(size: 36))
+                    List {
+                        ForEach($books) { $book in
+                            NavigationLink(value: $book.id) {
+                                BookRow(book: $book)
+                            }
+                        }
+                    }
                 }
             }
-            Button("Add sample book") {
-                books.append(Book(title: "Sample"))
+            .padding()
+            .navigationTitle("Book Tracker")
+            .toolbar {
+                Button(action: addNewBook) {
+                    Label("Add", systemImage: "plus")
+                }
+            }
+            .navigationDestination(for: UUID.self) { bookId in
+                if let index = books.firstIndex(where: { $0.id == bookId }) {
+                    BookFormView(book: $books[index])
+                        .navigationTitle("Edit Book")
+                        .navigationBarTitleDisplayMode(.inline)
+                }
             }
         }
-        .padding()
+    }
+    
+    func addNewBook() {
+        let newBook = Book(title: "New Book")
+        books.append(newBook)
+        path.append(newBook.id)
     }
 }
 
@@ -45,6 +84,13 @@ struct BookRow: View {
     var body: some View {
         Toggle(book.title, isOn: $book.isRead)
         
+    }
+}
+
+struct BookFormView: View {
+    @Binding var book: Book
+    var body: some View {
+        Text(book.title)
     }
 }
 
