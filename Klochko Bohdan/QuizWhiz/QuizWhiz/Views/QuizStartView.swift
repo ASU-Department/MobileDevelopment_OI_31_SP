57f7bd45e616232e96866333fc3442b2f1637d54
Fradid <bogd4n55@gmail.com>
2025-11-28 17:48:43 +0200

    LR3 Add networking, SwiftData persistence, offline mode and updated UI

--- Klochko Bohdan/QuizWhiz/QuizWhiz/Views/QuizStartView.swift
+++ Klochko Bohdan/QuizWhiz/QuizWhiz/Views/QuizStartView.swift
@@ -12,33 +12,11 @@ struct QuizStartView: View {
     var questionCount: Int
     var isHardMode: Bool
     
-    @State private var difficulty: Double = 0.5
-    @State private var showShare = false
-
     var body: some View {
-        VStack(spacing: 24) {
-
-            Text("Quiz Started")
-                .font(.largeTitle.bold())
-
-            Text("Category: \(category.name)")
-                .font(.title3)
-
-            // UIKit Slider
-            SliderView(value: $difficulty)
-                .frame(height: 40)
-                .padding()
-
-            Button("Share Result") {
-                showShare = true
-            }
-            .buttonStyle(.borderedProminent)
-            .sheet(isPresented: $showShare) {
-                ShareSheetController(activityItems: ["I started the quiz in \(category.name)!"])
-            }
-
-            Spacer()
-        }
-        .padding()
+        QuizView(
+            category: category,
+            questionCount: questionCount,
+            isHardMode: isHardMode
+        )
     }
 }
