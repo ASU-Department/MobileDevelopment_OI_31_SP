f95c3623c61424fd9d50a1fa6c9096a1170bf5c3
Pab1m <pab1m@Mac-Pab1m.local>
2025-12-14 14:57:17 +0200

    Lab4: Refactor app to MVVM with repository, actor and DI

--- Senchak Pavlo/SpaceExplorer2.0/SpaceExplorer2.0/Views/ExploreView.swift
+++ Senchak Pavlo/SpaceExplorer2.0/SpaceExplorer2.0/Views/ExploreView.swift
@@ -2,32 +2,18 @@ import SwiftUI
 
 struct ExploreView: View {
 
-    @State private var brightness: Double = 0.5
+    @StateObject private var viewModel = ExploreViewModel()
 
     var body: some View {
-        NavigationStack {
-            VStack {
-                Text("Explore Space")
-                    .font(.largeTitle.bold())
-                    .padding(.top, 20)
+        VStack {
+            Text("Explore Space")
+                .font(.largeTitle.bold())
 
-                Spacer()
+            BrightnessSlider(brightness: $viewModel.brightness)
 
-                Text("Adjust Star Brightness")
-                    .font(.headline)
-
-                BrightnessSlider(brightness: $brightness)
-                    .frame(height: 40)
-                    .padding(.horizontal, 40)
-
-                Image(systemName: "sparkles")
-                    .font(.system(size: 100))
-                    .foregroundColor(.yellow.opacity(brightness))
-                    .padding(.top, 20)
-
-                Spacer()
-            }
-            .padding()
+            Image(systemName: "sparkles")
+                .font(.system(size: 100))
+                .foregroundColor(.yellow.opacity(viewModel.brightness))
         }
     }
 }
