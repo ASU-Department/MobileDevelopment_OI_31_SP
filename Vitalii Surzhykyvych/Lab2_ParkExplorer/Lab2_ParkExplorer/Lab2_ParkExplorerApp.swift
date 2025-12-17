6fcea22fcb59a3b5ed269495259fa52956d85235
Vitalik Surzhykyvych <surzhykyvychv@gmail.com>
2025-12-17 03:26:50 +0200

    Init project from Lab 1

---
+++ Vitalii Surzhykyvych/Lab2_ParkExplorer/Lab2_ParkExplorer/Lab2_ParkExplorerApp.swift
@@ -0,0 +1,17 @@
+//
+//  Lab2_ParkExplorerApp.swift
+//  Lab2_ParkExplorer
+//
+//  Created by Vitalik on 17.12.2025.
+//
+
+import SwiftUI
+
+@main
+struct Lab2_ParkExplorerApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
