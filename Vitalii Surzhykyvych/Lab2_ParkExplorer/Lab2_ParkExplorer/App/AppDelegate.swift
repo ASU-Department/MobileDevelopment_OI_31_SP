34e6c806ec2784d80c322ef273ece82634c1ecda
Vitalik Surzhykyvych <surzhykyvychv@gmail.com>
2025-12-17 04:52:18 +0200

    Integrate AppDelegate with mock data initialization

---
+++ Vitalii Surzhykyvych/Lab2_ParkExplorer/Lab2_ParkExplorer/App/AppDelegate.swift
@@ -0,0 +1,20 @@
+//
+//  AppDelegate.swift
+//  Lab2_ParkExplorer
+//
+//  Created by Vitalik on 17.12.2025.
+//
+
+import UIKit
+
+class AppDelegate: NSObject, UIApplicationDelegate {
+
+    func application(
+        _ application: UIApplication,
+        didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey : Any]? = nil
+    ) -> Bool {
+
+        print("ParkExplorer started")
+        return true
+    }
+}
--- Vitalii Surzhykyvych/Lab2_ParkExplorer/Lab2_ParkExplorer/Lab2_ParkExplorerApp.swift
+++ Vitalii Surzhykyvych/Lab2_ParkExplorer/Lab2_ParkExplorer/Lab2_ParkExplorerApp.swift
@@ -9,6 +9,8 @@ import SwiftUI
 
 @main
 struct Lab2_ParkExplorerApp: App {
+    @UIApplicationDelegateAdaptor(AppDelegate.self) var appDelegate
+
     var body: some Scene {
         WindowGroup {
             ContentView()
