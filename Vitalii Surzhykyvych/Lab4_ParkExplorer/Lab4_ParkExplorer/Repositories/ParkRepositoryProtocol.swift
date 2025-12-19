88adc0e089b4068ef6c3975f0f6b453def869241
Vitalik Surzhykyvych <surzhykyvychv@gmail.com>
2025-12-19 02:38:49 +0200

    refactor: add ParkRepository abstraction with protocol and implementation

---
+++ Vitalii Surzhykyvych/Lab4_ParkExplorer/Lab4_ParkExplorer/Repositories/ParkRepositoryProtocol.swift
@@ -0,0 +1,12 @@
+//
+//  ParkRepositoryProtocol.swift
+//  Lab4_ParkExplorer
+//
+//  Created by Vitalik on 19.12.2025.
+//
+
+protocol ParkRepositoryProtocol {
+    func fetchParks() async throws -> [ParkAPIModel]
+    func loadCachedParks() throws -> [ParkAPIModel]
+    func saveParks(_ parks: [ParkAPIModel]) async
+}
