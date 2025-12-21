e86d67a6653287039d547a0e650047e0c0818bca
Vitalik Surzhykyvych <surzhykyvychv@gmail.com>
2025-12-21 21:13:23 +0200

    chore: split code into Models and Views folders

---
+++ Vitalii Surzhykyvych/ParkExplorer/ParkExplorer/Models/Park.swift
@@ -0,0 +1,18 @@
+//
+//  Park.swift
+//  ParkExplorer
+//
+//  Created by Vitalik on 17.12.2025.
+//
+
+import Foundation
+import MapKit
+
+struct Park: Identifiable {
+    let id = UUID()
+    let name: String
+    let state: String
+    let description: String
+    let coordinate: CLLocationCoordinate2D
+    var isFavorite: Bool = false
+}
